8da1b872b742d3378a9500a2dc783673842a9f26
Lucian Sturião Rodrigues <luciansr@gmail.com>
2015-07-25 20:30:04 -0300

    initial

---
+++ MinhaFolhaDePonto.App/public/App/MinhaFolhaDePonto.module.ts
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('MinhaFolhaDePonto', ['ngRoute']);
+
+    controller.$inject = ['$location'];
+
+    function controller($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'controller';
+
+        activate();
+
+        function activate() { }
+    }
+    
+})();
